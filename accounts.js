9028af79636526c459d512547c0d07922ca1423c
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-04-14 10:17:50 -0500

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -2,11 +2,32 @@ function acc
 {
 if(document.cookie == null)
 {
- 
+ var name = prompt("enter a username", "");
+ var pass = prompt("enter a password","");
+ if(true)//replace true with if account exists and if pass matches
+ {
+  sign_in()
+ }
+ else
+ {
+  var store = name+'|'+pass
+  document.cookie="person=" + store
+  //add something here that adds store to the .txt file
+ }
 }
   else
   {
-    let cook = document.cookie;
-    var f = cook.split('|')
+   if(true)//check if password matches
+    sign_in()
   }
+}
+function sign_in()
+{
+ var cookie=document.cookie
+ if(cookie.inculdes(';')){console.log('error code 1')}//cookie error
+ else
+ {
+  let cookies = cookie.split('|')
+  document.getElementById("name display").value = cookies[0];
+ }
 }
