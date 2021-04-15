823a02d13691b3ae9143d03caba3a2e869062114
Loki <71656278+underscoreL0K1@users.noreply.github.com>
2021-04-14 23:56:51 -0400

    Update index.php
    Set character limit to 224

--- index.php
+++ index.php
@@ -126,7 +126,7 @@
         
         <p id="subtext">Send Message as: $nickname</p>
         <form>
-            <center><textarea id="messagebox" maxlength = '700'></textarea></center>
+            <center><textarea id="messagebox" maxlength = '224'></textarea></center>
         </form>
     </div>
 </body>
